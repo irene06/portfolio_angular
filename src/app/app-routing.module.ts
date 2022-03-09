17be69f7deafcb79594f8ef7e3c254875176f053
Irene Escobar <irene.e06@gmail.com>
2022-03-08 23:47:39 -0300

    definiendo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',component:PortfolioComponent},
+  { path:'about', component:AboutComponent},
+  { path:'item', component:ItemComponent},
+  { path:'**',pathMatch:'full', redirectTo:''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
