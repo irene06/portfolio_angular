af45e7ebfdb02f71189e3c329f3469654f85ec6d
Irene Escobar <irene.e06@gmail.com>
2022-03-09 01:57:46 -0300

    interfaces

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,8 @@
+  export interface InfoPagina {
+        titulo?: string;
+        email?: string;
+        nombre_corto?: string;
+        instagram?: string;
+        facebook?: string;
+    }
+
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {InfoPagina} from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  info: any ={};
+  info: InfoPagina ={};
   cargada =false;
 
   constructor( private http: HttpClient ) {
 
   this.http.get('assets/data-pagina.json')
-        .subscribe( resp  => {
+        .subscribe( (resp:InfoPagina)  => {
           this.cargada = true;
           this.info = resp;
         console.log( resp );
